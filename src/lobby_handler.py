e2804e9063af44129bb81a04d0376aa160eafeca
Donaim <3Donaim@gmail.com>
2020-01-21 14:26:59 +0100

    Add initial implementation of LobbyHandler and LobbyInterface
    Adding this to test base_handler and base_interface modules
    
    To send requests do:
    ```bash
    $ python3 lobby_handler & # starts server
    $ python3
    >>> import lobby_interface
    >>> lobby_interface.LobbyInterface.ping()
    {'uuid': '9ec9deeb-5adb-45c7-9dad-3d6b21428c92'}
    >>> lobby_interface.LobbyInterface.echo('hello')
    {'value': 'hello'}
    ```

---
+++ src/lobby_handler.py
@@ -0,0 +1,20 @@
+
+import uuid
+import base_handler
+import constant
+
+class LobbyHandler:
+	def __init__(self, public_uuid):
+		self.public_uuid = public_uuid
+
+	def echo(self, what_to_echo):
+		return {'value': what_to_echo}
+
+	def ping(self):
+		return {'uuid': str(self.public_uuid)}
+
+if __name__ == '__main__':
+	base_handler.run(
+		LobbyHandler(uuid.uuid4()),
+		address=constant.LOBBY_DOMAIN_NAME,
+		port=constant.LOBBY_PORT)
---
+++ src/lobby_interface.py
@@ -0,0 +1,10 @@
+
+import base_interface
+import constant
+
+@base_interface.decorator(constant.LOBBY_URL)
+class LobbyInterface:
+	def echo(what_to_echo):
+		pass
+	def ping():
+		pass
