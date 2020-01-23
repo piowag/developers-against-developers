5d003f8c361e95af25b31fab45addbde2de459b3
Daniel Bober <danielmbober@gmail.com>
2020-01-24 00:46:08 +0100

    Fix server interface creation

--- src/game_server_interface.py
+++ src/game_server_interface.py
@@ -1,53 +1,55 @@
 
-class GameServerInterface:
+def create_game_server_interface():
     """
     Call like:
-    interface = base_interface.decorator("http:/...")(GameServerInterface)
+    interface = base_interface.decorator("http:/...")(create_game_server_interface())
     """
+    class GameServerInterface:
 
-    def add_player_to_game(token):
-        """
-        Called by lobby.
-        Adds player with given id.
-        """
+        def add_player_to_game(token):
+            """
+            Called by lobby.
+            Adds player with given id.
+            """
 
-    def get_player_role(token):
-        """
-        Called by all players.
-        Returns current role ("gm" or "player").
-        """
+        def get_player_role(token):
+            """
+            Called by all players.
+            Returns current role ("gm" or "player").
+            """
 
-    def start_game():
-        """
-        Called by all players.
-        Starts game, joining is not possible afterwards.
-        """
+        def start_game():
+            """
+            Called by all players.
+            Starts game, joining is not possible afterwards.
+            """
 
-    def get_task():
-        """
-        Called by GM and players.
-        """
+        def get_task():
+            """
+            Called by GM and players.
+            """
 
-    def send_answer(token, code_file):
-        """
-        Called by players.
-        Sends player's code to the game server.
-        """
+        def send_answer(token, code_file):
+            """
+            Called by players.
+            Sends player's code to the game server.
+            """
 
-    def get_answers_from_players(token):
-        """
-        Called by GM.
-        Collects all answers sent by players.
-        """
+        def get_answers_from_players(token):
+            """
+            Called by GM.
+            Collects all answers sent by players.
+            """
 
-    def choose_winner(token, winner_token):
-        """
-        Called by GM.
-        Select code to be run on a VM.
-        """
+        def choose_winner(token, winner_token):
+            """
+            Called by GM.
+            Select code to be run on a VM.
+            """
 
-    def get_round_results(token):
-        """
-        Called by GM and Players.
-        Returns scores after a round.
-        """
+        def get_round_results(token):
+            """
+            Called by GM and Players.
+            Returns scores after a round.
+            """
+    return GameServerInterface
