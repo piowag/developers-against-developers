bece3e1a9d430e9128015d898c94b3eb81f61b31
Donaim <3Donaim@gmail.com>
2020-01-25 00:35:20 +0100

    create servers

--- src/lobby_handler.py
+++ src/lobby_handler.py
@@ -22,17 +22,31 @@ class LobbyHandler:
 		servers = list(self.k8s.list_game_servers())
 		responses = []
 
-		for addr in servers:
-			serv = create_game_server_interface_by_address(addr)
-			serv_response = serv.initialize_new_game()
-			responses.append(serv_response)
-			if response_is_ok(serv_response):
-				return serv_response
+		def loop():
+			for addr in servers:
+					serv = create_game_server_interface_by_address(addr)
+					serv_response = serv.initialize_new_game()
+					responses.append(serv_response)
+					if response_is_ok(serv_response):
+						return serv_response
 
-		return {'status': constant.STATUS_ERROR,
-		        'msg': 'servers are busy',
-		        'servers': str(servers),
-		        'responses': str(responses)}
+		resp = loop()
+		if resp:
+			return resp
+		else:
+			if not self.k8s.create_game_server():
+				return {'status': constant.STATUS_ERROR,
+				        'msg': 'could not create new server',
+				        'servers': str(servers),
+				        'responses': str(responses)}
+			resp = loop()
+			if resp:
+				return resp
+			else:
+				return {'status': constant.STATUS_ERROR,
+				        'msg': 'servers are busy',
+				        'servers': str(servers),
+				        'responses': str(responses)}
 
 	def add_me_to_server(self, server_public_url):
 		for addr in self.k8s.list_game_servers():
