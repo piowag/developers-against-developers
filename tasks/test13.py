5aaa388f02e1ad3e5b8625205086773e20792627
Daniel Bober <danielmbober@gmail.com>
2020-06-13 13:23:02 +0200

    Add two easy questions

---
+++ tasks/test13.py
@@ -0,0 +1,11 @@
+# 2. Create a directory called "CreateMe".
+
+import os.path
+from os import path
+
+def main():
+
+    assert os.path.exists('~/task_one')
+
+if __name__== "__main__":
+   main()
---
+++ tasks/test14.py
@@ -0,0 +1,11 @@
+import os.path
+from os import path
+
+def main():
+
+    assert os.path.exists('~/task_two.txt')
+    f = open("~/task_two.txt", "r")
+    assert(f.read() == "5")
+
+if __name__== "__main__":
+       main()
